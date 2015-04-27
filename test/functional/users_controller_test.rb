8933593b2f209bb69650840789d34c5e2484d9a0
geordie <george.speake@gmail.com>
2015-04-27 08:44:55 -0700

    change travis config to speed things up (don't need all the different rubys tested). add json format to users index.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -11,6 +11,25 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should get json list of users' do
+    user = create(:user)
+    sign_in(user)
+
+    get :index, {:format => 'json'}
+
+    assert_response :success
+
+    json_response_as_hash = JSON.parse(@response.body)
+
+    assert_equal 1, json_response_as_hash.count
+
+    user_1 = json_response_as_hash.first
+
+    assert_equal user.id,    user_1['id']
+    assert_equal user.email, user_1['email']
+    assert_equal user.name,  user_1['name']
+  end
+
   test "should get show" do
     user = create(:user)
     sign_in(user)
